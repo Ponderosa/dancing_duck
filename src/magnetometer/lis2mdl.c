6dcd63dd5b219442dd5a966c5ff65136b26d9053
Josh Erickson <earle.ponderosa@gmail.com>
2024-07-06 13:47:18 -0700

    feat: add printf switch to lis2mdl

--- src/magnetometer/lis2mdl.c
+++ src/magnetometer/lis2mdl.c
@@ -7,6 +7,8 @@
 #include "lis2mdl.h"
 #include "task.h"
 
+#define PRINT_DEBUG     0
+
 #define I2C_BAUD        (1000 * 1000)
 #define I2C_TIMEOUT_US  (1000)
 #define I2C_ADDRESS     (0x1E)
@@ -16,7 +18,7 @@
 static const uint8_t WHO_AM_I_ADDRESS = 0x4F;
 static const uint8_t WHO_AM_I_ID = 0x40;
 static const uint8_t CONFIG_CONTINUOUS[2] = {0x60, 0x00};
-static const uint8_t OUT_ADDRESS = 0x67;
+static const uint8_t OUT_ADDRESS = 0x68;
 
 static float binary_to_ut(int16_t in) {
   static const float factor = GAUSS_RANGE * ONE_GAUSS_IN_UT / INT16_MAX;
@@ -48,17 +50,28 @@ MagXYZ get_xyz_uT() {
   MagXYZ ret_val = {0.0, 0.0, 0.0};
 
   i2c_write_timeout_us(&i2c0_inst, I2C_ADDRESS, &OUT_ADDRESS, 1, true, I2C_TIMEOUT_US);
-  i2c_read_timeout_us(&i2c0_inst, I2C_ADDRESS, &in_buffer[0], 9, false, I2C_TIMEOUT_US);
-  printf("LIS2MDL Status: 0x%02hhX\n", in_buffer[0]);
-  int16_t mag = in_buffer[1] | (in_buffer[2] << 8);
+  i2c_read_timeout_us(&i2c0_inst, I2C_ADDRESS, &in_buffer[0], 8, false, I2C_TIMEOUT_US);
+
+  // X
+  int16_t mag = in_buffer[0] | (in_buffer[1] << 8);
   ret_val.x_uT = binary_to_ut(mag);
-  printf("LIS2MDL OUT X: %d\n", mag);
-  mag = in_buffer[3] | (in_buffer[4] << 8);
+  if (PRINT_DEBUG) {
+    printf("LIS2MDL OUT X: %d\n", mag);
+  }
+
+  // Y
+  mag = in_buffer[2] | (in_buffer[3] << 8);
   ret_val.y_uT = binary_to_ut(mag);
-  printf("LIS2MDL OUT Y: %d\n", mag);
-  mag = in_buffer[5] | (in_buffer[6] << 8);
+  if (PRINT_DEBUG) {
+    printf("LIS2MDL OUT Y: %d\n", mag);
+  }
+
+  // Z
+  mag = in_buffer[4] | (in_buffer[5] << 8);
   ret_val.z_uT = binary_to_ut(mag);
-  printf("LIS2MDL OUT Z: %d\n", mag);
+  if (PRINT_DEBUG) {
+    printf("LIS2MDL OUT Z: %d\n", mag);
+  }
 
   return ret_val;
 }
