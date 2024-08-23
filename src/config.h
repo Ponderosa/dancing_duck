e8ad1a16d126fe513d362be048ec3c5d0c3ff378
Josh Erickson <earle.ponderosa@gmail.com>
2024-08-22 20:37:15 -0700

    chore: update fw version

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 #define _DD_CONFIG_H
 
 // Firmware Version
-static const uint32_t FIRMWARE_VERSION = 5;
+static const uint32_t FIRMWARE_VERSION = 6;
 
 // Watchdog
 static const uint32_t WATCHDOG_DELAY_MS = 2000;
